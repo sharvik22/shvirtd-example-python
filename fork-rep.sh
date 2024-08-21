f29631cdb1a989228a3e68bf201c507ae803d0a1
VIKTOR <sharvik22@mail.ru>
2024-08-21 23:49:21 +0300

    edit fork-rep

--- fork-rep.sh
+++ fork-rep.sh
@@ -6,10 +6,6 @@ if ! command -v docker &> /dev/null; then
     sudo apt install -y docker docker-compose-v2
 fi
 
-if ! command -v docker-compose &> /dev/null; then
-    sudo apt install -y docker-compose
-fi
-
 # Проверка и клонирование/обновление репозитория
 if [ ! -d "/opt/shvirtd-example-python" ]; then
     sudo git clone https://github.com/sharvik22/shvirtd-example-python.git /opt/shvirtd-example-python
@@ -20,4 +16,4 @@ fi
 
 # Запуск проекта с помощью Docker Compose
 cd /opt/shvirtd-example-python
-sudo docker-compose -f compose.yaml up -d
+sudo docker compose -f compose.yaml up -d
